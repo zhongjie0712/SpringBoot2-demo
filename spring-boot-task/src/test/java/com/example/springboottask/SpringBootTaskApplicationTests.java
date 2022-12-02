d46d252e480cde88bf91e77b6ae8ac323be46ad7
zhongjie <zhongjie0712@qq.com>
2022-12-02 23:52:16 +0800

    first commit v1

---
+++ spring-boot-task/src/test/java/com/example/springboottask/SpringBootTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-boot-transaction/src/main/java/com/example/springboottransaction/SpringBootTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTransactionApplication.class, args);
+    }
+
+}
